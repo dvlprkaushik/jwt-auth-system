40627481577d3ee3012dd8a1d30971a521937fd4
kaushik <dvlprkaushik@gmail.com>
2025-11-06 19:44:34 +0530

    fix : type assertions

--- src/env/env.ts
+++ src/env/env.ts
@@ -20,13 +20,13 @@ export const env = {
   nodeEnv: process.env.NODE_ENV || "development",
   baseUrl: process.env.BASE_URL || `http://localhost:${Number(process.env.PORT)}`,
   // 🧩 Database
-  dbUrl: process.env.DATABASE_URL as string,
+  dbUrl: process.env.DATABASE_URL!,
 
   // 🔐 JWT Config
-  accessSecret: process.env.ACCESS_SECRET as string,
-  refreshSecret: process.env.REFRESH_SECRET as string,
-  accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || "15m",
-  refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || "7d",
+  accessSecret: process.env.ACCESS_SECRET!,
+  refreshSecret: process.env.REFRESH_SECRET!,
+  accessTokenExpiresIn: (process.env.ACCESS_TOKEN_EXPIRES_IN ?? "15m") as string | number,
+  refreshTokenExpiresIn: (process.env.REFRESH_TOKEN_EXPIRES_IN ?? "7d") as string | number,
 
   // 🧂 Bcrypt
   bcryptSaltRounds: Number(process.env.BCRYPT_SALT_ROUNDS) || 10,
